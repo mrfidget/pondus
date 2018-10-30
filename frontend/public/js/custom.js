792bf32c726fc098a8d224303cd9c622403a346e
mrfidget <ferdie@ferdie.co.uk>
2018-10-30 19:27:48 +0000

    #3 more changes to js #3 add suppressing custom css

--- frontend/public/js/custom.js
+++ frontend/public/js/custom.js
@@ -1,12 +1,51 @@
-$(function () {
-    // build the call to the REST API
+function setShowSubmitErrorMsg(flag) {
+    if (flag) {
+        $("#submitError").show();
+    } else {
+        $("#submitError").hide();
+    }
+
+}
+function validateInput(formData) {
+    if (typeof formData != 'undefined') {
+        if(formData != ''){
+            return true;
+        }
+    }else{        
+        return false;
+    }
+};
+function toUpperCaseFirst(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+}
+
+$(function () {    
     $("#logit").submit(function (event) {
-        console.log('I is in');
+        // there is no error
+        setShowSubmitErrorMsg(false);
         // don't go anywhere
         event.preventDefault();
-        //collect the data
+        // collect the data
         var data = $("#logit :input").serializeArray();
-        console.log(data[0].value);
-//dologit
+        var name = data[0].value;
+        var kilo = data[1].value;
+        var stone = data[2].value;
+        // perform some validation
+        var isValidForm;
+        // data.forEach(element => {
+        //     isValidForm = isValidForm + validateInput(element);
+        // });
+        if(isValidForm){
+            console.log('valid');
+        }else{
+            setShowSubmitErrorMsg(true);  
+        }
+        name = toUpperCaseFirst(name);
+        //var cleanData = data.forEach(validateInput);
+        // set a timestamp
+
+        // send it to ELK
+        console.log(name,kilo,stone);
+        //dologit
     });
 });
